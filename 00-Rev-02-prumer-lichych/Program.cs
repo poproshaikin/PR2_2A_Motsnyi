0669b9a300e65eba1ca6ae4b32b6322ba743c64c
Stanislav <stasstrong06@gmail.com>
2024-10-01 09:14:42 +0200

    Prumer lichych

---
+++ 00-Rev-02-prumer-lichych/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _00_Rev_02_prumer_lichych;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> kladnaLicha = new();
+        Console.WriteLine("Uvadejte cisla");
+
+        while (true)
+        {
+            string vstup = Console.ReadLine()!;
+            if (vstup == "stop")
+                break;
+
+            bool parsed = int.TryParse(vstup, out int result);
+
+            if (!parsed)
+            {
+                Console.WriteLine("Je to spatne cislo ale jedem dal");
+            }
+            else
+                if (result % 2 == 1 && result >= 0)
+                    kladnaLicha.Add(result);
+        }
+        
+        Console.WriteLine($"Prumer kladnych lichych cisel: {kladnaLicha.Average()}");
+    }
+}
