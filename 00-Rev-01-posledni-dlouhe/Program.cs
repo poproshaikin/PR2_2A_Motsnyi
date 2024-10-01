9e68fc3f225d3bb9ad4152ab03f3f501f5ba600c
Stanislav <stasstrong06@gmail.com>
2024-10-01 09:00:20 +0200

    Posledni dlouhe

---
+++ 00-Rev-01-posledni-dlouhe/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] slova = { "Karel", "Iva", "Jaromir", "Eva", "Martin" };
+        Console.WriteLine(PosledniDlouhe(slova, 5));
+        Console.WriteLine(PosledniDlouhe(slova, 7));
+        Console.WriteLine(PosledniDlouhe(slova, 12));
+    }
+
+    private static string PosledniDlouhe(string[] slova, int limit)
+    {
+        return slova.LastOrDefault(s => s.Length >= limit) ?? "";
+    }
+}
